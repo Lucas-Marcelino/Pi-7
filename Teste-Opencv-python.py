159f9247d7de02c19e63ff709d9be71329fddc68
Lucas Marcelino <37718741+Lucas-Marcelino@users.noreply.github.com>
2021-04-15 20:14:39 -0300

    Atualizacao

--- Teste-Opencv-python.py
+++ Teste-Opencv-python.py
@@ -12,4 +12,6 @@ cv2.imshow('Teste opencv', img)	# Mostra a janela criada(1º parâmetro) e atrib
 cv2.waitKey() 				# Após criar janela com img, aguarda alguma tecla para break
 								# waitKey('milisegundos')
 
-cv2.destroyAllWindows()
+cv2.destroyAllWindows() 
+
+#ofkdos 
